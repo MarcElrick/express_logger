d738ea86fa3c9ece8576852b13b1f8668e568f17
Marc Elrick <marcelrick15@hotmail.com>
2022-01-17 15:25:52 +0000

    Switched to WebSocket API -works better

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-import express from "express";
-import ip from "ip";
+import WebSocket, { WebSocketServer } from "ws";
 import fs from "fs";
-import cors from "cors";
 
-const app = express();
-app.use(cors());
-const port = 3000;
 const filename = "log.json";
 
-app.get("/", (request, response) => {
-  fs.readFile(filename, "utf8", (err, data) => {
-    if (err) {
-      onsole.log(err);
-      return;
-    }
+const server = new WebSocketServer({ port: 9000 });
+server.on("connection", (socket) => {
+  console.log("connection");
 
-    const content = JSON.parse(data);
-    if (request.query.data) {
-      console.log(request.query.data);
-      const newData = JSON.parse(request.query.data);
+  socket.on("message", (message) => {
+    console.log(ab2str(message));
+
+    fs.readFile(filename, "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      try {
+        const content = JSON.parse(data);
+        const newData = JSON.parse(ab2str(message));
+        const key = Object.keys(newData)[0];
+        console.log(key);
+        content.logs[key] = newData[key];
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+      const content = JSON.parse(data);
+      const newData = JSON.parse(ab2str(message))["data"];
       const key = Object.keys(newData)[0];
       console.log(key);
       content.logs[key] = newData[key];
-    }
-    const writeStream = fs.createWriteStream(filename, { flags: "w" });
-    writeStream.write(JSON.stringify(content));
-    writeStream.end();
 
-    response.send("Hello World!");
+      const writeStream = fs.createWriteStream(filename, { flags: "w" });
+      writeStream.write(JSON.stringify(content));
+      writeStream.end();
+    });
+    socket.send("roger that");
   });
 });
 
-app.listen(port, () => {
-  console.log(`Logger listening at http://${ip.address()}:${port}`);
-});
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
